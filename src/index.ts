0ea5842a23b3c3108cd1bfcfb390937ffc311fd0
adrianhopebailie <adrian@hopebailie.com>
2018-07-19 00:09:28 +0200

    bug: remove custom property from opts
    prevent conflict if similar named property is added to ioredis in future

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,7 @@ class RedisStore {
 
   constructor (opts: StoreOptions) {
     this._prefix = 'ilp:' + (opts.prefix || '') + ':'
+    delete(opts.prefix)
     this._redis = new Redis(Object.assign({ keyPrefix: this._prefix }, opts))
   }
 
